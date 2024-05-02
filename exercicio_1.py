f52992ec3338765820b1145ad5e6df6c696f479d
arthurfs11 <arthur_faria_santos@hotmail.com>
2024-05-02 13:59:37 -0300

    corrigido resposta final

--- exercicio_1.py
+++ exercicio_1.py
@@ -15,12 +15,14 @@ planilha_work = planilha['Sheet1'] # ATRIBUI A PLANILHA SHEET1 (ONDE ESTÃO AS I
 
 # PASSO 3 - ABRIR SITE E INICIAR DESAFIO
 driver.get('https://www.rpachallenge.com/') # ABRE O LINK DO DESAFIO
-time.sleep(5) # AGUARDA 5 SEGUNDOS PARA A PÁGINA CARREGAR
+time.sleep(3) # AGUARDA 3 SEGUNDOS PARA A PÁGINA CARREGAR 
 driver.find_element(By.XPATH, "//button[contains(text(),'Start')]").click() # CLICA NO BOTÃO START
 time.sleep(1) # AGUARDA 1 SEGUNDO PARA INICIAR O DESAFIO
 
 # PASSO 4 - CADASTRAR INFORMAÇÕES
 for i in planilha_work.iter_rows(min_row=2, values_only=True):
+    if not i[0]:
+        continue
     driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelFirstName']").send_keys(i[0]) # PREENCHE O FIRST NAME
     driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelLastName']").send_keys(i[1]) # PREENCHE O LAST NAME
     driver.find_element(By.XPATH, "//input[@ng-reflect-name='labelCompanyName']").send_keys(i[2]) # PREENCHE COMPANY NAME
@@ -31,5 +33,5 @@ for i in planilha_work.iter_rows(min_row=2, values_only=True):
     driver.find_element(By.XPATH, "/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input").click() # CLICA EM SUBMIT
 
 # PASSO 5 - OBTER O RESULTADO E EXIBIR O TEMPO QUE LEVOU PARA CADASTRAR
-resultado = driver.find_element(By.XPATH, "/html/body/app-root/div[2]/app-rpa1/div/div[2]/div[2]").get_attribute('InnerHTML') # OBTEM O RESULTADO
+resultado = driver.find_element(By.XPATH, "/html/body/app-root/div[2]/app-rpa1/div/div[2]/div[2]").text # OBTEM O RESULTADO
 print(resultado)
